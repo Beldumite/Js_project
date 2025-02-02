4a23d7c90d6c5241f7d883f1a967cb22ad4797c0
Kutuputih <azkaarrofiq12@gmail.com>
2025-02-03 01:13:59 +0700

    update

--- p20/scirpt.js
+++ p20/scirpt.js
@@ -1,53 +1,60 @@
 const list = document.querySelector("ul");
-
-const textColorRadio = document.querySelectorAll('[name = "textColor"]');
-const hoverColorRadio = document.querySelectorAll('[name = "hoverColor"]');
+const textColorRadio = document.querySelectorAll('[name="textColor"]');
+const hoverColorRadio = document.querySelectorAll('[name="hoverColor"]');
 let lastColor = "black";
+let currentHoverColor = null;
 
 function updateStatus() {
   list.innerHTML = "";
-  const ContentInput = document.querySelector("#content");
-  const NumInput = document.querySelector("#num");
+  const contentInput = document.querySelector("#content");
+  const numInput = document.querySelector("#num");
 
-  for (let i = 0; i < NumInput.value; i++) {
-    const ListChild = document.createElement("li");
-    ListChild.textContent = ContentInput.value + (i + 1);
-    list.appendChild(ListChild);
+  for (let i = 0; i < numInput.value; i++) {
+    const listChild = document.createElement("li");
+    listChild.textContent = contentInput.value + (i + 1);
+    list.appendChild(listChild);
   }
+
+  if (lastColor) changeColor(lastColor);
+  if (currentHoverColor) setupHoverEffects();
 }
 
-// warna text
-
 function changeColor(color) {
-  const listItem = document.querySelectorAll("ul li");
-  listItem.forEach((li) => {
+  const listItems = document.querySelectorAll("ul li");
+  listItems.forEach((li) => {
     li.style.color = color;
+  });
+}
+
+function setupHoverEffects() {
+  const listItems = document.querySelectorAll("ul li");
+  listItems.forEach((li) => {
+    li.removeEventListener("mouseover", li.hoverInHandler);
+    li.removeEventListener("mouseout", li.hoverOutHandler);
+
+    li.hoverInHandler = () => {
+      li.style.color = currentHoverColor;
+    };
+
+    li.hoverOutHandler = () => {
+      li.style.color = lastColor;
+    };
+
+    li.addEventListener("mouseover", li.hoverInHandler);
+    li.addEventListener("mouseout", li.hoverOutHandler);
   });
 }
 
 textColorRadio.forEach((radioColor) => {
   radioColor.addEventListener("change", () => {
-    changeColor(radioColor.value);
     lastColor = radioColor.value;
+    changeColor(lastColor);
   });
 });
 
-// warna hover
-
-function changeHover(color) {
-  const listItem = document.querySelectorAll("ul li");
-  listItem.forEach((li) => {
-    li.addEventListener("mouseover", () => {
-      li.style.color = color;
-    });
-    li.addEventListener("mouseout", () => {
-      li.style.color = lastColor;
-    });
-  });
-}
-
 hoverColorRadio.forEach((hoverColor) => {
   hoverColor.addEventListener("change", () => {
-    changeHover(hoverColor.value);
+    currentHoverColor = hoverColor.value;
+    setupHoverEffects();
   });
 });
